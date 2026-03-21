7083fc5b444a52b7fabe37340c1b45a17d071b40
vivekcse123 <vermavivek0402@gmail.com>
2026-03-22 01:31:56 +0530

    Changes in angular.json buddget and app.routes.server.ts file

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,8 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: 'welcome',       renderMode: RenderMode.Prerender },
+  { path: 'auth/login',    renderMode: RenderMode.Prerender },
+  { path: 'auth/register', renderMode: RenderMode.Prerender },
+  { path: '**',            renderMode: RenderMode.Server },
 ];
