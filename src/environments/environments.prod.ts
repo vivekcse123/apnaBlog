8ff3b8b5f3b71b76631c80c426cd96d7edcec46b
vivekcse123 <vermavivek0402@gmail.com>
2026-04-04 00:27:17 +0530

    changes in domain

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://apnablogserver.onrender.com/api',
-  clientApiUrl: 'apan-blog.vercel.app',
+  clientApiUrl: 'https://apnablogs.vercel.app',
   
   // Legacy endpoints (if you're still using them elsewhere)
   apiAuthEndpoint: 'https://apnablogserver.onrender.com/api/auth/',
