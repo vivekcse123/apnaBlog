f687234ba4d4381f3c79d5f3ba2711d15e23ec15
vivekcse123 <vermavivek0402@gmail.com>
2026-04-12 10:58:51 +0530

    final change in bio

--- src/app/features/user/models/user.mode.ts
+++ src/app/features/user/models/user.mode.ts
@@ -7,12 +7,12 @@ export interface User {
   location:     string;   
   role:         string;
   status:       string;
+  bio: string;
   totalBlogs:   number;
   totalViews:   number;
   createdAt:    Date;
   updatedAt:    Date;
   lastLoggedInAt: Date;
-  deletionScheduledAt: any
-  avatar: string,
-  bio: string
+  deletionScheduledAt: any;
+  avatar: string
 }
